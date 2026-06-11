5e2d61d76cc224a0916e85d1112b4ddce6644d1b
Mrutyunjaya <satapathymrutyunjaya915@gmail.com>
2026-06-11 12:42:46 +0530

    add vercel URL to CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ import { setupWebSocketServer } from './ws/wsServer.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://remote-term.vercel.app',], credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
